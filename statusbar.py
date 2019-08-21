e846de53b99d6c0b94c4f2c1bb1adfac967b2e2a
Jonas Krüger Svensson <jonas-ks@hotmail.com>
2019-08-21 16:28:37 +0200

    Less lines of code doing the same work

--- statusbar.py
+++ statusbar.py
@@ -20,32 +20,24 @@ def write(data: str):
 
 
 def fetch_data():
-    name = pwd.getpwuid( os.getuid() ).pw_name
+    name = pwd.getpwuid(os.getuid()).pw_name
     disk_root = bytes2human(disk_usage('/').free)
     disk_home = bytes2human(disk_usage(f'/home/{name}').free)
     # ip = gethostbyname(gethostname()) # Unused for now
     try:
         ssid = check_output('nmcli -g CONNECTION,TYPE d | grep wifi', shell=True).strip().decode('utf-8').split(':')[0]
         wireless_info = check_output(f'nmcli -t device wifi | grep {ssid}', shell=True).strip().decode('utf-8')
-        wireless_as_list = wireless_info.split(':')
-        bars_in_stars = wireless_as_list[6].count('*')
-        if bars_in_stars == 4:
-            bars = '▂▄▆█'
-        elif bars_in_stars == 3:
-            bars = '▂▄▆_'
-        elif bars_in_stars == 2:
-            bars = '▂▄__'
-        elif bars_in_stars == 1:
-            bars = '▂___'
-        else:
-            bars = '____'
+        bars_in_stars = wireless_info.split(':')[6].count('*')  # e.g. signal is *** out of ****
+        bars = '▂▄▆█'  # Just prettier formatting
+        current_bars = bars[0:bars_in_stars]  # Translate stars to bars
+        formatted_bars = current_bars + ''.join(['_' for x in range(4 - bars_in_stars)])
     except Exception:
         ssid = ''
-        bars = ''
+        formatted_bars = ''
     battery = int(sensors_battery().percent)
     status = 'Charging' if sensors_battery().power_plugged else 'Discharging'
     date = datetime.now().strftime('%h %d %A %H:%M')
-    formatted = f'💾: {disk_root}/home/{disk_home} | {ssid} {bars} | 🔋: {battery}% {status} | {date}'
+    formatted = f'💾: {disk_root}/home/{disk_home} | {ssid} {formatted_bars} | 🔋: {battery}% {status} | {date}'
     return formatted
 
 
